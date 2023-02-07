82620a9ae56044411240e5c840913d7249dd95db
Rei Ameku <e225756@eve.u-ryukyu.ac.jp>
2023-02-07 20:35:41 +0900

    1th

--- Report5/src/jp/ac/uryukyu/ie/e225756/Report5.java
+++ Report5/src/jp/ac/uryukyu/ie/e225756/Report5.java
@@ -3,11 +3,7 @@ package jp.ac.uryukyu.ie.e225756;
 public class Report5 {
     public static void main(String[] args){
         String str = null;
-        try{
-System.out.println(str.length());            
-        }catch(NullPointerException e){
-            System.out.println("nullPointExceptionが存在しています");            
-System.out.println(e.getMessage()) ;           
+        System.out.println(str.length());
         }
-    }
 }
+        
